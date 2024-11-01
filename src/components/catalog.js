165e7fce4b6591a4e90ed3bf6718fbd2bb2efa10
MartsivMaria <152096237+MartsivMaria@users.noreply.github.com>
2024-11-01 19:18:12 +0200

    Add files via upload

---
+++ src/components/catalog.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Chainsaws from "./chainsaws";
+
+const Catalog = () => {
+    return (
+        <div>
+            <Chainsaws />
+            <button className="view-btn">View more</button>
+        </div>
+    )
+}
+
+export default Catalog; 
---
+++ src/components/navigation.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navigation = () => {
+    return (
+        <nav className="navigation">
+            <h1 className="nav-title">TimberPro</h1>
+            <ul>
+                <li>Home</li>
+                <li>Catalog</li>
+                <li>Cart</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navigation;
